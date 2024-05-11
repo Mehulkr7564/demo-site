740bdec635c9d4ad09fcd516b2f93251706fb565
Mehul Kumar <mehulkumar12042003@gmail.com>
2024-05-12 02:26:50 +0530

    Add files via upload

---
+++ simon.js
@@ -0,0 +1,37 @@
+  let gameSeq=[];
+  let userSeq=[];
+
+  let btns=["yellow","red","green","blue"];
+
+  let started = false;
+  let level=0;
+
+  let h2 = document.querySelector("h2");
+
+  document.addEventListener("keypress", function(){
+    if (started==false){
+        console.log("Game Started");
+        started = true;
+
+        levelUp();
+    }
+});
+function levelUp(){
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    },250);
+}
+
+
+function levelUp(){
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btns[randIdx];
+    let randBtn = documents.querySelector(`.${randColor}`);
+
+    btnFlash(randBtn);
+}
+
